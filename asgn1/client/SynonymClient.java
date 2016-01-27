d0b318c6cf3f81397167a05d69eedb9b2c9a12a2
George Lifchits <george.lifchits@gmail.com>
2016-01-27 12:06:39 -0500

    de-static the SynonymClient class

--- asgn1/client/SynonymClient.java
+++ asgn1/client/SynonymClient.java
@@ -4,33 +4,33 @@ import java.net.*;
 
 public class SynonymClient {
 
-    private static Socket socket;
-    private static PrintWriter output;
-    private static BufferedReader input;
+    private Socket socket;
+    private PrintWriter output;
+    private BufferedReader input;
 
-    public static boolean connect(String ipAddress, int portNumber) throws Exception {
+    public boolean connect(String ipAddress, int portNumber) throws Exception {
         socket = new Socket(ipAddress, portNumber);
         output = new PrintWriter(socket.getOutputStream());
         input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         return true;
     }
 
-    public static boolean isConnected() {
+    public boolean isConnected() {
         return socket != null;
     }
 
-    private static void throwIfNotConnected() throws Exception {
+    private void throwIfNotConnected() throws Exception {
         if (!isConnected()) {
             throw new Exception("You are not connected to the server");
         }
     }
 
-    private static void sendDisconnectToServer() throws Exception {
+    private void sendDisconnectToServer() throws Exception {
         throwIfNotConnected();
         output.println(); // sends an empty line, signals disconnect
     }
 
-    public static void disconnect() throws Exception {
+    public void disconnect() throws Exception {
         throwIfNotConnected();
         sendDisconnectToServer();
         socket.close();
@@ -38,15 +38,15 @@ public class SynonymClient {
 
     /* Methods for interacting with the Synonym Protocol */
 
-    public static void get(String getWord) {
+    public void get(String getWord) {
         System.out.println("Get word: " + getWord);
     }
 
-    public static void set(String word1, String word2) {
+    public void set(String word1, String word2) {
         System.out.println("Set Synonyms: " + word1 + " is a synonym for " + word2);
     }
 
-    public static void remove(String removeWord) {
+    public void remove(String removeWord) {
         System.out.println("Remove word: " + removeWord);
     }
 
