9fce4bd8b534a4cfffb339d2dbe782e440f8faeb
George Lifchits <george.lifchits@gmail.com>
2016-03-24 18:38:27 -0400

    simplify indentation and cleanup superfluous comments

--- asgn3/client/PaintPanel.java
+++ asgn3/client/PaintPanel.java
@@ -5,42 +5,38 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.JPanel;
 
-public class PaintPanel extends JPanel
-{
-   private int pointCount = 0; // count number of points
+public class PaintPanel extends JPanel {
 
-   // array of 10000 java.awt.Point references
-   private Point points[] = new Point[ 10000 ];
+    private int pointCount = 0; // count number of points
 
-   // set up GUI and register mouse event handler
-   public PaintPanel()
-   {
-      // handle frame mouse motion event
-      addMouseMotionListener(
+    // array of 10000 java.awt.Point references
+    private Point points[] = new Point[ 10000 ];
 
-         new MouseMotionAdapter() // anonymous inner class
-         {
-            // store drag coordinates and repaint
-            public void mouseDragged( MouseEvent event )
-            {
-               if ( pointCount < points.length )
-               {
-                  points[ pointCount ] = event.getPoint(); // find point
-                  pointCount++; // increment number of points in array
-                  repaint(); // repaint JFrame
-               } // end if
-            } // end method mouseDragged
-         } // end anonymous inner class
-      ); // end call to addMouseMotionListener
-   } // end PaintPanel constructor
+    private PaintClient client;
 
-   // draw oval in a 4-by-4 bounding box at specified location on window
-   public void paintComponent( Graphics g )
-   {
-      super.paintComponent( g ); // clears drawing area
+    // set up GUI and register mouse event handler
+    public PaintPanel() {
+        this.addMouseMotionListener(
+            new MouseMotionAdapter() {
+                // store drag coordinates and repaint
+                public void mouseDragged( MouseEvent event ) {
+                    if ( pointCount < points.length )  {
+                        points[ pointCount ] = event.getPoint(); // find point
+                        pointCount++;
+                        repaint();
+                    }
+                }
+            }
+        );
+    }
 
-      // draw all points in array
-      for ( int i = 0; i < pointCount; i++ )
-         g.fillOval( points[ i ].x, points[ i ].y, 4, 4 );
-   } // end method paint
-} // end class PaintPanel
+    // draw oval in a 4-by-4 bounding box at specified location on window
+    public void paintComponent( Graphics g ) {
+        super.paintComponent( g ); // clears drawing area
+
+        // draw all points in array
+        for ( int i = 0; i < pointCount; i++ ) {
+            g.fillOval( points[ i ].x, points[ i ].y, 4, 4 );
+        }
+    }
+}
